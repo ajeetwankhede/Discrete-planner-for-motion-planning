77eff52d475c0abdc88f07641534c73647323c85
ajeetwankhede <ajeet@umd.edu>
2018-11-11 17:17:26 -0500

    Added Doxygen style comments.

--- app/main.cpp
+++ app/main.cpp
@@ -1,5 +1,13 @@
-#include <Map.hpp>
-#include <iostream>
+/**
+ *  @copyright MIT License 2018 Ajeet Wankhede
+ *  @file    main.cpp
+ *  @author  Ajeet Wankhede
+ *  @date    11/09/2018
+ *  @version 1.0
+ *  @brief   Discrete planner for motion planning
+ *  @section DESCRIPTION
+ *  main source file.
+ */
 
 int main() {
     return 0;
