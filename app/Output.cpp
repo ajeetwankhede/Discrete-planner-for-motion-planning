742406eed6fc3cb83e3498b7a089aa21874e32f9
ajeetwankhede <ajeet@umd.edu>
2018-11-11 17:08:52 -0500

    Task: CCS Subtask: CDCS Created source file of class Ouput containing class method definitions.

---
+++ app/Output.cpp
@@ -0,0 +1,32 @@
+/**
+ *  @copyright MIT License 2018 Ajeet Wankhede
+ *  @file    Output.cpp
+ *  @author  Ajeet Wankhede
+ *  @date    11/10/2018
+ *  @version 1.0
+ *  @brief   Discrete planner for motion planning
+ *  @section DESCRIPTION
+ *  Output class definition of methods.
+ */
+
+#include <vector>
+#include <utility>
+#include "Output.hpp"
+
+using std::vector;
+using std::pair;
+
+Output::Output() {
+  // Initializing values to the attributes of Map class
+
+}
+
+Output::~Output() {
+  // Destructor stub
+}
+
+void Output::writeTextFile(vector<pair<int, int> > path) {
+}
+
+void Output::showOutput(vector<pair<int, int> > path) {
+}
