fded303b8366b2d01e0daab2d5f37dfe44286ece
ajeetwankhede <ajeet@umd.edu>
2018-11-23 15:32:19 -0500

    Task: CTS Created a main.cpp file for testing.

--- test/main.cpp
+++ test/main.cpp
@@ -1,3 +1,14 @@
+/**
+ *  @copyright MIT License 2018 Ajeet Wankhede
+ *  @file    main.cpp
+ *  @author  Ajeet Wankhede
+ *  @date    11/09/2018
+ *  @version 1.0
+ *  @brief   Discrete planner for motion planning
+ *  @section DESCRIPTION
+ *  main source file for testing.
+ */
+
 #include <gtest/gtest.h>
 
 int main(int argc, char** argv) {
