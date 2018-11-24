81f3e80b520ff48ae41cd45662ecccffb978282f
ajeetwankhede <ajeet@umd.edu>
2018-11-24 17:55:19 -0500

    Task: ICL Subtask: IMCS Edited actionTest case to remove extra while loop.

--- test/MapTest.cpp
+++ test/MapTest.cpp
@@ -41,14 +41,10 @@ TEST(Map, verifyNodesTest) {
  */
 TEST(Map, actionTest) {
   mapTestObject = std::make_shared<Map>();
-  int i = 1;
   pair<int, int> currentNode = make_pair(5, 5);
   // Run all the four actions and verify that they give the desired results
-  while (i < 5) {
-    EXPECT_EQ(make_pair(6, 5), mapTestObject->action(i, currentNode));
-    EXPECT_EQ(make_pair(5, 6), mapTestObject->action(i, currentNode));
-    EXPECT_EQ(make_pair(4, 5), mapTestObject->action(i, currentNode));
-    EXPECT_EQ(make_pair(5, 4), mapTestObject->action(i, currentNode));
-    i++;
-  }
+  EXPECT_EQ(make_pair(6, 5), mapTestObject->action(1, currentNode));
+  EXPECT_EQ(make_pair(5, 6), mapTestObject->action(2, currentNode));
+  EXPECT_EQ(make_pair(4, 5), mapTestObject->action(3, currentNode));
+  EXPECT_EQ(make_pair(5, 4), mapTestObject->action(4, currentNode));
 }
